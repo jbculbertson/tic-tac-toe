7b1fb6b714355a049094e2d75047368acfbe74d6
jbculbertson <jbculbertson@gmail.com>
2017-07-08 13:47:57 -0400

    Added header with title.  Improved UI with messaging

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -12,20 +12,18 @@ const signUpFailure = () => {
 const signInSuccess = (data) => {
   store.user = data.user
   $('#message-banner').text('Welcome back ' + store.user.email + '.  Please click Create Game.')
-  $('#create-game-button').css('display', 'inline')
-  $('#sign-out-button').css('display', 'inline')
-  $('#show-games-button').css('display', 'inline')
-  $('#change-password-button').css('display', 'inline')
+  $('.logged-in').css('display', 'inline')
   $('#sign-up-button').hide()
   $('#sign-in-button').hide()
+  $('#stats-banner').css('display', 'block')
 }
 const signInFailure = () => {
   $('#message-banner').text('Please make sure you have the correct credentials')
 }
 
 const signOutSuccess = () => {
-  $('#message-banner').text('See you next time')
 }
+
 const signOutFailure = () => {
 }
 
@@ -38,10 +36,8 @@ const changePasswordFailure = () => {
 
 const createGameSuccess = (data) => {
   store.game = data.game
-  $('#message-banner').text('Let\'s Play!')
-  $('#stats-banner').css('display', 'inline')
-  $('#stats-banner').text('Stats go here')
-  $('.gameboard').fadeIn(500).css('display', 'inline')
+  $('#message-banner').text('User ' + store.user.email + ' goes first, as X')
+  $('.gameboard').fadeIn(500).css('display', 'block')
 }
 
 const createGameFailure = () => {
